55f865231a2076c20d485abeb9203bd615971df7
Andrew <andriypituley@gmail.com>
2020-10-21 00:31:23 +0300

    Added role/level enums, added base for user editing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: [:member, :admin]
+
+  enum level: [:trainee, :junior, :middle, :senior]
+
+  after_initialize :set_default_level, :set_default_role, :if => :new_record?
+
+  def set_default_level
+    self.level ||= :junior
+  end
+
+  def set_default_role
+    self.role ||= :member
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+
+  get 'user/edit' => 'user#edit'
+
+  patch 'user/update' => 'user#update'
+
+  #get 'user/:id' => 'user#show', as: 'user_path'
+
+  resources :user
 end
