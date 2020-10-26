ea066942e761affb37fc204e350609a4107fedca
Andrew <andriypituley@gmail.com>
2020-10-26 13:21:24 +0200

    Unite user editing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include Pundit
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name, :avatar])
+  end
 end
