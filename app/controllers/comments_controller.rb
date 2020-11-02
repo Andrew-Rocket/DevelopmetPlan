cfc7543c5d00b297e7b293a922baa8021a5e1d61
Andrew <andriypituley@gmail.com>
2020-11-02 12:53:42 +0200

    Fixed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
   def create
     @task = Task.find(params[:task_id])
     @comment = @task.comments.build(comment_params)
-    @comment.user_id= current_user.id
+    @comment.user = current_user
 
     flash[:notice] = 'Something went wrong' unless @comment.save
 
